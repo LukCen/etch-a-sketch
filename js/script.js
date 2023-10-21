3a48dde1ba258b2f97fb65e03da12dcea571ce57
Lukasz Cena <lbjcena@gmail.com>
2023-10-21 19:18:30 +0200

    added user prompt, board generates based on user input now

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 'use strict'
 
 const main = document.querySelector('main')
-function testDraw (num) {
-  for (let i = 0; i < num; i++) {
+const ask = prompt('How many elements do you want on your grid?')
+
+const rootOfNumber = Math.floor(Math.pow(ask, 1 / 2)) // this should make sure we only get integers I think
+
+main.style.gridTemplateColumns = `repeat(${rootOfNumber}, 50px)`
+main.style.gridTemplateRows = `repeat(${rootOfNumber}, 50px)`
+
+function testDraw (ask = 0) {
+  for (let i = 0; i < ask; i++) {
     const n = document.createElement('div')
     n.classList.add('draw-cell')
     main.append(n)
   }
 }
 
-testDraw(16 * 16)
+testDraw(ask)
 const cell = document.querySelectorAll('.draw-cell')
 const cellArray = Array.from(cell)
 cellArray.forEach((c) => {
